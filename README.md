d105a3c7d7f8d6f5d4d623b83a0198d17b0370ac
TanJiaLong <jialong999999@gmail.com>
2023-12-31 09:39:01 +0800

    JL Update

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+## Output 1:
+![Output 1](resources/output1.png)
+
+## Output 2:
+![Output 2](resources/output2.png)
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ public class Main {
         String inputRelation = scanner.next() + scanner.nextLine();
 
         // Step 3: Generate the matrix
-        int[][] matrix = generateMatrix(inputRelation);
+        String[][] matrix = generateMatrix(inputRelation);
 
         // Step 4: Display the matrix
         if (matrix != null) {
@@ -24,40 +24,41 @@ public class Main {
         }
     }
 
+    //Accept alphabet and number
     // Function to generate the matrix from the input relation
-    private static int[][] generateMatrix(String inputRelation) {
-
+    private static String[][] generateMatrix(String inputRelation) {
         String[] pairs = convertToArray(inputRelation);
+        Set<String> domainSet = new HashSet<>();
+        Set<String> rangeSet = new HashSet<>();
 
-        int maxDomain = 0;
-        int maxRange = 0;
-
-        for (String s : pairs) {
-            String[] elements = s.split(",");
-            int curDomain = Integer.parseInt(elements[0]);
-            int curRange = Integer.parseInt(elements[1]);
-
-            maxDomain = Math.max(maxDomain, curDomain);
-            maxRange = Math.max(maxRange, curRange);
+        for (String pair : pairs) {
+            String[] elements = pair.split(",");
+            domainSet.add(elements[0]);
+            rangeSet.add(elements[1]);
         }
+        String[] domainArray = domainSet.toArray(new String[0]);
+        String[] rangeArray = rangeSet.toArray(new String[0]);
 
         // Create the matrix
-        int[][] matrix = new int[maxDomain][maxRange];
+        String[][] matrix = new String[domainArray.length][rangeArray.length];
 
         // Fill the matrix based on the input relation
         for (String pair : pairs) {
             String[] elements = pair.split(",");
-            int a = Integer.parseInt(elements[0]) - 1;  // Adjust index to start from 0
-            int b = Integer.parseInt(elements[1]) - 1;  // Adjust index to start from 0
-            matrix[a][b] = 1;  // Mark the position in the matrix
+            String a = elements[0];
+            String b = elements[1];
+            int rowIndex = Arrays.asList(domainArray).indexOf(a);
+            int colIndex = Arrays.asList(rangeArray).indexOf(b);
+            matrix[rowIndex][colIndex] = "1";  // Mark the position in the matrix
         }
 
-        if(maxDomain == 0){
+        if (domainArray.length == 0 || rangeArray.length == 0) {
             return null;
-        }else{
+        } else {
             return matrix;
         }
     }
+
 
     private static String[] convertToArray(String inputRelation) {
         // Extract pairs from the input relation
@@ -77,12 +78,49 @@ public class Main {
 
 
     // Function to print the matrix
-    private static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.print(value + " ");
+    private static void printMatrix(String[][] matrix) {
+        for (String[] row : matrix) {
+            for (String value : row) {
+                System.out.print(value != null ? value : "0");
+                System.out.print(" ");
             }
             System.out.println();
         }
     }
 }
+
+// Accept number only
+//    // Function to generate the matrix from the input relation
+//    private static int[][] generateMatrix(String inputRelation) {
+//
+//        String[] pairs = convertToArray(inputRelation);
+//
+//        int maxDomain = 0;
+//        int maxRange = 0;
+//
+//        for (String s : pairs) {
+//            String[] elements = s.split(",");
+//            int curDomain = Integer.parseInt(elements[0]);
+//            int curRange = Integer.parseInt(elements[1]);
+//
+//            maxDomain = Math.max(maxDomain, curDomain);
+//            maxRange = Math.max(maxRange, curRange);
+//        }
+//
+//        // Create the matrix
+//        int[][] matrix = new int[maxDomain][maxRange];
+//
+//        // Fill the matrix based on the input relation
+//        for (String pair : pairs) {
+//            String[] elements = pair.split(",");
+//            int a = Integer.parseInt(elements[0]) - 1;  // Adjust index to start from 0
+//            int b = Integer.parseInt(elements[1]) - 1;  // Adjust index to start from 0
+//            matrix[a][b] = 1;  // Mark the position in the matrix
+//        }
+//
+//        if(maxDomain == 0){
+//            return null;
+//        }else{
+//            return matrix;
+//        }
+//    }
